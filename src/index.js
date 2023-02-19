d8d8dc3025ef5555dc15604e69c4a11fd6072749
aryang23 <aryangrover230402@gmail.com>
2023-02-19 12:19:54 +0530

    Body Parser added

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+    const app = express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+    });
+}
+
+setupAndStartServer();
